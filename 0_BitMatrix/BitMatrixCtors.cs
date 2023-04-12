3c0278a366c5a3aad823c3591eab363c5ca17db6
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-04-12 19:41:54 +0200

    add constructors

--- 0_BitMatrix/BitMatrixCtors.cs
+++ 0_BitMatrix/BitMatrixCtors.cs
@@ -1,28 +1,86 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _0_BitMatrix
+
+namespace BitMatrixNameSpace
 {
-    internal partial class BitMatrix
+    public partial class BitMatrix
     {
         public BitMatrix(int numberOfRows, int numberOfColumns, params int[] bits)
         {
-            NumberOfColumns = numberOfColumns;
+            data = new BitArray(numberOfRows * numberOfColumns);
+
+            if (bits != null && bits.Length > 0)
+            {
+                for (int i = 0, k = 0; i < numberOfRows; i++)
+                {
+                    for (int j = 0; j < numberOfColumns; j++, k++)
+                    {
+                        if (k < bits.Length)
+                            data[i * numberOfColumns + j] = BitToBool(bits[k]);
+                        else
+                            data[i * numberOfColumns + j] = false;
+                    }
+                }
+            }
+
             NumberOfRows = numberOfRows;
-
-            if (bits is not null)
-            {
-
-            }
-            else
-            {
-
-            }
-
-
+            NumberOfColumns = numberOfColumns;
         }
+
+        public BitMatrix(int[,] bits)
+        {
+            if (bits == null)
+                throw new NullReferenceException();
+
+            int numberOfRows = bits.GetLength(0);
+            int numberOfColumns = bits.GetLength(1);
+
+            if (bits.Length == 0)
+                throw new ArgumentOutOfRangeException();
+
+            data = new BitArray(numberOfRows * numberOfColumns);
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                for (int j = 0; j < numberOfColumns; j++)
+                {
+                    data[i * numberOfColumns + j] = BitToBool(bits[i, j]);
+                }
+            }
+
+            NumberOfRows = numberOfRows;
+            NumberOfColumns = numberOfColumns;
+        }
+
+        public BitMatrix(bool[,] bits)
+        {
+            if (bits == null)
+                throw new NullReferenceException();
+
+            int numberOfRows = bits.GetLength(0);
+            int numberOfColumns = bits.GetLength(1);
+
+            if (bits.Length == 0)
+                throw new ArgumentOutOfRangeException();
+
+            data = new BitArray(numberOfRows * numberOfColumns);
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                for (int j = 0; j < numberOfColumns; j++)
+                {
+                    data[i * numberOfColumns + j] = bits[i, j];
+                }
+            }
+
+            NumberOfRows = numberOfRows;
+            NumberOfColumns = numberOfColumns;
+        }
+
     }
 }
