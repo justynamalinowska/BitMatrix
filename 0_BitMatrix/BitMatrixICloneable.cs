3e1973741ed80e942926b80656ba420133614548
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-04-13 12:58:08 +0200

    add interface ICloneable

---
+++ 0_BitMatrix/BitMatrixICloneable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BitMatrixNameSpace
+{
+    public partial class BitMatrix : ICloneable
+    {
+        public object Clone()
+        {
+            BitMatrix clonedMatrix = new BitMatrix(NumberOfRows, NumberOfColumns);
+            for (int i = 0; i < NumberOfRows; i++)
+            {
+                for (int j = 0; j < NumberOfColumns; j++)
+                {
+                    clonedMatrix[i, j] = this[i, j];
+                }
+            }
+            return clonedMatrix;
+        }
+    }
+}
